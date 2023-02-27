a5a7e04dbcd902b7c84727c8ac0a5bc617e1b541
Kijeee02 <110998028+Kijeee02@users.noreply.github.com>
2023-02-27 14:13:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     API_KEY_RM_BG: "yfYFEWvNUHGkkE9dfSdSQt2t",
-    API_KEY_OPEN_AI: "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"
+    API_KEY_OPEN_AI: "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"
 }
